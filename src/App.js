c03e970611a8075de0713117617d264882a51b8c
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-16 03:20:58 +0500

    Add logic folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import Calculator from './components/Calculator.js';
+import calculate from './components/calculate.js';
 
 class App extends React.Component {
   render() {
