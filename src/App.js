078e8e6dae8ec9d9c9e3d9d8820ecc245025983c
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-15 21:34:42 +0500

    Create UI for calculator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Calculator from './components/Calculator.js';
 
 function App() {
+  const newCalculator = new Calculator();
   return (
-  
+    newCalculator.render()
   );
 }
 
