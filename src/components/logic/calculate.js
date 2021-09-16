2462c7243639614637a7f68486324ca841885e11
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-16 21:55:06 +0500

    Make required changes

--- src/components/logic/calculate.js
+++ src/components/logic/calculate.js
@@ -16,7 +16,7 @@ function isNumber(item) {
 export default function calculate(obj, buttonName) {
   if (buttonName === 'AC') {
     return {
-      total: null,
+      total: 0,
       next: null,
       operation: null,
     };
