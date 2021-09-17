f439627347bf00e08ee30a1f3783377892ee8e3a
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-17 05:23:07 +0500

    Add hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,27 @@
-/* eslint-disable linebreak-style */
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import './css/calculator.css';
-import calculate from './logic/calculate';
+import calculate from './logic/calculate.js';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleEvent = this.handleEvent.bind(this);
-  }
+const Calculator = () => {
+  const [calculator, addState] = useState({ total: 0, next: null, operation: null });
+  const { total, next, operation } = calculator;
 
-  handleEvent = (e) => {
+  const handleEvent = (e) => {
     e.preventDefault();
-    this.setState((prev) => calculate(prev, e.target.name));
+    addState((prev) => calculate(prev, e.target.name));
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    const showTotal = () => {
-      if (total === 0 && next === null) {
-        return 0;
-      }
-      if (total !== 0 && total !== null) {
-        return total;
-      }
-      return '';
-    };
-    return (
+  const showTotal = () => {
+    if (total === 0 && next === null) {
+      return 0;
+    }
+    if (total !== 0 && total !== null) {
+      return total;
+    }
+    return '';
+  };
+  return (
       <div className="calculator-grid">
         <div className="output">
           <input
@@ -41,71 +32,70 @@ class Calculator extends React.Component {
             disabled
           />
         </div>
-        <button className="data-all-clear" onClick={this.handleEvent} name="AC">
+        <button className="data-all-clear" onClick={handleEvent} name="AC">
           AC
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="+/-">
+        <button className="data-number" onClick={handleEvent} name="+/-">
           +/-
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="%">
+        <button className="data-number" onClick={handleEvent} name="%">
           %
         </button>
-        <button className="data-operation" onClick={this.handleEvent} name="÷">
+        <button className="data-operation" onClick={handleEvent} name="÷">
           ÷
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="1">
+        <button className="data-number" onClick={handleEvent} name="1">
           1
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="2">
+        <button className="data-number" onClick={handleEvent} name="2">
           2
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="3">
+        <button className="data-number" onClick={handleEvent} name="3">
           3
         </button>
-        <button className="data-operation" onClick={this.handleEvent} name="*">
+        <button className="data-operation" onClick={handleEvent} name="*">
           *
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="4">
+        <button className="data-number" onClick={handleEvent} name="4">
           4
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="5">
+        <button className="data-number" onClick={handleEvent} name="5">
           5
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="6">
+        <button className="data-number" onClick={handleEvent} name="6">
           6
         </button>
-        <button className="data-operation" onClick={this.handleEvent} name="+">
+        <button className="data-operation" onClick={handleEvent} name="+">
           +
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="7">
+        <button className="data-number" onClick={handleEvent} name="7">
           7
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="8">
+        <button className="data-number" onClick={handleEvent} name="8">
           8
         </button>
-        <button className="data-number" onClick={this.handleEvent} name="9">
+        <button className="data-number" onClick={handleEvent} name="9">
           9
         </button>
-        <button className="data-operation" onClick={this.handleEvent} name="-">
+        <button className="data-operation" onClick={handleEvent} name="-">
           -
         </button>
         <button
           className="data-number"
-          onClick={this.handleEvent}
+          onClick={handleEvent}
           name="0"
           id="span-two"
         >
           0
         </button>
-        <button className="data-number" onClick={this.handleEvent} name=".">
+        <button className="data-number" onClick={handleEvent} name=".">
           .
         </button>
-        <button className="data-operation" onClick={this.handleEvent} name="=">
+        <button className="data-operation" onClick={handleEvent} name="=">
           =
         </button>
       </div>
-    );
-  }
-}
+  );
+};
 
 export default Calculator;
